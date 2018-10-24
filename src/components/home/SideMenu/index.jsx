ae61d3b4cea0423daa33833ead9341edbf2d2f2a
ram16230 <ram16230@uvg.edu.gt>
2018-10-24 16:17:23 -0600

    maquetacion

--- src/components/home/SideMenu/index.jsx
+++ src/components/home/SideMenu/index.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import './side-menu.css';
 
+import MenuItem from '../SideMenuItem';
+
 const SideMenu = () => (
 
     <div className="sideMenu">
-    
+        <ul className="menuList">
+            <MenuItem icon="far fa-sticky-note" name="All notes" />
+        </ul>
     </div>
 
 );
