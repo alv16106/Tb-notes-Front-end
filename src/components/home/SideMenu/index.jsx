7e29263a464d2acd563084988663fc1fe180c1cf
ram16230 <ram16230@uvg.edu.gt>
2018-10-25 14:42:53 -0600

    Side menu

--- src/components/home/SideMenu/index.jsx
+++ src/components/home/SideMenu/index.jsx
@@ -9,7 +9,44 @@ const SideMenu = () => (
     <div className="sideMenu">
         <ul className="menuList">
             <MenuItem icon="far fa-sticky-note" name="All notes" />
+            <MenuItem icon="far fa-trash-alt" name="Trash" />
         </ul>
+
+        <ul className="sideMenu folders scroll" id="style-1"> 
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+            <MenuItem name="bla"/>
+        </ul>
+
     </div>
 
 );
--- src/components/home/SideMenuItem/index.jsx
+++ src/components/home/SideMenuItem/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './side-menu-item.css';
 
-const SideMenuItem = ({ icon, name }) => (
+const SideMenuItem = ({ icon = "far fa-folder", name }) => (
     <li className="itemContainer">
         <a className="item">
             <i className={`icon ${icon}`}></i>
