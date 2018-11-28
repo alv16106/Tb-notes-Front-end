0d63c199b3fb395bfabeb205b284463c257cd5d3
ram16230 <ram16230@uvg.edu.gt>
2018-11-28 13:26:49 -0600

    ADD Logout sin ningun estilo

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import * as routes from '../../constants/routes';
+import * as actions from '../../actions';
 
 const Navigation = ({ authUser }) =>
     <div className="nav-wrapper" id="cssmenu">
@@ -12,12 +13,22 @@ const Navigation = ({ authUser }) =>
       }
     </div>
 
-const NavigationAuth = () =>
+const NavigationAuthDummy = ({logout}) =>
   <ul id="nav-mobile" className="right">
     <li><NavLink to={routes.LANDING} activeClassName="active">Home</NavLink></li>
     <li><NavLink to={routes.ACCOUNT} activeClassName="active">Cuenta</NavLink></li>
+    <li onClick={event => logout()} ><NavLink to={routes.LANDING}  activeClassName="active" > <i class="fas fa-sign-out-alt"></i> </NavLink></li>
     &nbsp;
   </ul>
+
+const NavigationAuth = connect(
+  undefined,
+  dispatch => ({
+    logout: () => {
+      dispatch(actions.logoutSuccess())
+    },
+  })
+)(NavigationAuthDummy)
 
 const NavigationNonAuth = () =>
   <ul id="nav-mobile" className="right">
@@ -29,4 +40,7 @@ const mapStateToProps = (state) => ({
   authUser: state.user.token,
 });
 
-export default connect(mapStateToProps)(Navigation);
+export default connect(
+  mapStateToProps,
+  undefined
+)(Navigation);
